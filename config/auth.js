b0e4cd8b224d4b12c6736b8d3bc8998b0c5019a9
Adam Crabtree <dude@noderiety.com>
2015-05-13 21:13:12 -0700

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,25 @@
+// config/auth.js
+module.exports = {
+  'development': {
+    'facebook': {
+      'consumerKey': '...',
+      'consumerSecret': '...',
+      'callbackUrl': '...'
+    },
+    'linkedin': {
+      'consumerKey': '...',
+      'consumerSecret': '...',
+      'callbackUrl': 'http://social-authenticator.com:8000/auth/linkedin/callback'
+    },
+    'twitter': {
+      'consumerKey': 'OaPwwyfXjd8WkPzPEWPlpT6Fs',
+      'consumerSecret': '...',
+      'callbackUrl': 'http://social-authenticator.com:8000/auth/twitter/callback'
+    },
+    'google': {
+      'consumerKey': '446585441765-unda5mjs6307q1pqobvhiqj87m9m2kh1.apps.googleusercontent.com',
+      'consumerSecret': '...',
+      'callbackUrl': 'http://social-authenticator.com:8000/auth/google/callback'
+    }
+  }
+}
